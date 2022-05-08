7ea297c3a7c47503a74aff3e1568f016c7b1ca15
dsampath <dsampath@wiley.com>
2022-05-08 14:18:21 +0530

    Basic UI completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import { Fragment } from "react";
 import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meals";
 
+//render as a self loading component
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
---
+++ src/component/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react"; //as a wrapper component
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />
+      <AvailableMeals />
+    </Fragment>
+  );
+};
+
+export default Meals;
