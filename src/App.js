22e1f5fdb5ab3f1025b707314ac68dcc56466881
dsampath <dsampath@wiley.com>
2022-05-10 13:10:41 +0530

    card loading on backdrop functionality added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./component/Layout/Header";
 import Meals from "./component/Meals/Meals";
 import Cart from "./component/Cart/Cart";
 
 //Header as a self loading component
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
@@ -17,3 +27,9 @@ function App() {
 }
 
 export default App;
+
+//now we need to manage states.
+//1. cart is visible
+//2. cart is not visible
+//we can wrap between curly braces to evaluate dynamic conditions
+//important; dont execute as showCartHandler()
