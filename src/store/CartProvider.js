bdfddf3f14c9fdfe3742b3176ef7511d94a7e65c
dsampath <dsampath@wiley.com>
2022-05-11 15:54:04 +0530

    cart reducer added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,46 @@
 //the goal of this component is simply to manage the cart context data and provide that data to all components that want access to it.
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+//we are going to manage complex states using useReducer
+import {useReducer} from "react";
 
-  const removeItemToCartHandler = (id) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+};
+
+//we have two kinds of actions which trigger this reducer
+//concat() gives a brand new array instead of editing existing one
+const cartReducer = (state, action) => {
+  if(action.type === 'ADD'){
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    //return the new state
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  //useReducer retrun an array with exactly two elements
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  //we need to make sure this 'addItemToCartHandler' is called from some component(meals component) with appropriate
+  // data(i.e. id/name/price/amount)
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({type:'ADD', item:item});
+  };
+
+  const removeItemToCartHandler = (id) => {
+    dispatchCartAction({type:'REMOVE', id:id});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler
   };
