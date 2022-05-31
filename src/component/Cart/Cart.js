4f9b772a751ded277d9bf41dba68c96e0df8b621
dsampath <dsampath@wiley.com>
2022-05-31 12:45:37 +0530

    input form added v1

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,52 +1,65 @@
-import {useContext} from "react";
+import {useContext, useState} from "react";
 
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
+import Checkout from "./Checkout";
 
 const Cart = (props) => {
+    const [isCheckout, setIsCheckout] = useState(false);
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveHandler = id => {};
+    const cartItemRemoveHandler = id => {
+        setIsCheckout(true);
+    };
     const cartItemAddHandler = item => {
-        cartCtx.addItem({...item, amount:1});
+        cartCtx.addItem({...item, amount: 1});
     };
 
-  //we want to map all cartItems to cartItem.jsx elements
-  const cartItems = (
-    <ul className={classes["cart-items"]}>
-      {cartCtx.items.map((item) => (
-          <CartItem
-              key={item.id}
-              name={item.name}
-              amount={item.amount}
-              price={item.price}
-              onRemove={cartItemRemoveHandler.bind(null, item.id)}
-              onAdd={cartItemAddHandler.bind(null, item.item)}
-          />
-      ))}
-    </ul>
-  );
+    //we want to map all cartItems to cartItem.jsx elements
+    const cartItems = (
+        <ul className={classes["cart-items"]}>
+            {cartCtx.items.map((item) => (
+                <CartItem
+                    key={item.id}
+                    name={item.name}
+                    amount={item.amount}
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item.item)}
+                />
+            ))}
+        </ul>
+    );
 
-  return (
-    <Modal onClose={props.onClose}>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>
-          Close
-        </button>
-          {hasItems && <button className={classes.button}>Order</button>}
-      </div>
-    </Modal>
-  );
+    const orderHander = () => {
+        setIsCheckout(true);
+    }
+
+    const modelActions = (
+        <div className={classes.actions}>
+            <button className={classes["button--alt"]} onClick={props.onClose}>
+                Close
+            </button>
+            {hasItems && <button className={classes.button} onClick={orderHander}>Order</button>}
+        </div>
+    )
+
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>{totalAmount}</span>
+            </div>
+            {isCheckout && <Checkout onCancel={props.onClose}/>}
+            {!isCheckout && modelActions}
+        </Modal>
+    );
 };
 
 export default Cart;
